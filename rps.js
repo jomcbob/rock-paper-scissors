0a2d0a94d77c80e7c611ed1bdb0abe3d599adfb1
andy <andrew@risertech.com>
2024-09-10 15:53:01 -0600

    made game function for a round

--- rps.js
+++ rps.js
@@ -3,21 +3,60 @@ console.log("Hello World!");
 //Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
 //Test that function returns what I expect using console.log
 
-function getComputerChoice() {
-   let choice = Math.floor(Math.random() * 10);
-    if (choice <= 3) {
-        return("scissors");
-    } else if (choice <= 6 && choice > 3) {
-        return("rock");
-    } else if (choice <= 9 && choice > 6) {
-        return("paper");
+
+   let getComputerChoice = Math.floor(Math.random() * 10);
+    if (getComputerChoice <= 3) {
+        getComputerChoice = ("scissors");
+    } else if (getComputerChoice <= 6 && getComputerChoice > 3) {
+        getComputerChoice = ("rock");
+    } else if (getComputerChoice <= 9 && getComputerChoice > 6) {
+        getComputerChoice = ("paper");
     }
-    }
+    
 
 // make a function called getHumanChoice that gets input from the user 
 // use prompt to tell user in input something
 //Test that function returns what I expect using console.log
 
+let getHumanChoice = prompt("type rock, paper, or scissors, in the box below please.").toLowerCase();
 
-    console.log(prompt("type rock, paper, or scissors, in the box below please.") + ", your choice")
-    console.log(getComputerChoice() + ", computer choice");
+
+    //console.log(getHumanChoice() + ", your choice");
+   // console.log(getComputerChoice() + ", computer choice");
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // make a new function call playRound that takes robot and human choices and says who wins
+    // make it so that even if they put in RoCk it still works
+    // increase humanScore or computerScore based on who won
+
+    function playRound(humanChoice, computerChoice) {
+        if (getComputerChoice == "paper" && getHumanChoice == "rock") {
+            return("you lose paper beats rock");
+        } else if (getComputerChoice == "paper" && getHumanChoice == "paper") {
+            return("you tie you both picked paper");
+        } else if (getComputerChoice == "paper" && getHumanChoice == "scissors") {
+            return("you win! scissors beats paper");
+        } else if (getComputerChoice == "rock" && getHumanChoice == "rock") {
+            return("you tie you both picked rock");
+        } else if (getComputerChoice == "rock" && getHumanChoice == "paper") {
+            return("you win! paper beats rock");
+        } else if (getComputerChoice == "rock" && getHumanChoice == "scissors") {
+            return("you lose rock beats scissors"); 
+        } else if (getComputerChoice == "scissors" && getHumanChoice == "rock") {
+            return("you win! rock beats scissors");
+        } else if (getComputerChoice == "scissors" && getHumanChoice == "paper") {
+            return("you lose scissors beats paper");
+        } else if (getComputerChoice == "scissors" && getHumanChoice == "scissors") {
+        } else return("did not enter a choice try another choice");
+        }
+
+
+    console.log(getHumanChoice + ", your choice");
+    console.log(getComputerChoice + ", computers choice");
+    console.log(playRound());
+
+
+
+ 
