7b41e232a48b95b3848e17095db1b031c2ecc117
andy <andrew@risertech.com>
2024-10-14 12:11:18 -0600

    swiched from being played with alerts to buttons

--- rps.js
+++ rps.js
@@ -1,7 +1,4 @@
 console.log("Hello World!");
-//Create a new function named getComputerChoice.
-//Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.
-//Test that function returns what I expect using console.log
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 10);
@@ -16,13 +13,9 @@ function getComputerChoice() {
     return (computerChoice);
 }
 
-// make scores to keep track of points
 let humanScore = 0;
 let computerScore = 0;
 
-// make a new function call playRound that takes robot and human choices and says who wins
-// make it so that even if they put in RoCk it still works
-// increase humanScore or computerScore based on who won
 
 function playRound(humanChoice, computerChoice) {
     if (computerChoice == "paper" && humanChoice == "rock") {
@@ -48,33 +41,92 @@ function playRound(humanChoice, computerChoice) {
         ++computerScore
         return ("you lose scissors beats paper");
     } else if (computerChoice == "scissors" && humanChoice == "scissors") {
-        return("you tie you both picked scissors");
+        return ("you tie you both picked scissors");
     } else return ("nice try enter a vaild option");
 
 }
 
 
 
-//get the result of five rounds of the game. the player choice and computer choice displays on the console
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = prompt('Enter rock, paper, or scissors:').toLowerCase();
-        const computerChoice = getComputerChoice();
-        console.log(`You chose: ${humanChoice}`);
-        console.log(`The computer chose: ${computerChoice}`);
-        console.log(playRound(humanChoice, computerChoice));
-        console.log(humanScore + " your score");
-        console.log(computerScore + " computer's score");
+
+
+humanChoice = 0;
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener("click", () => {
+    humanChoice = "rock";
+    const computerChoice = getComputerChoice();
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`The computer chose: ${computerChoice}`);
+    console.log(playRound(humanChoice, computerChoice));
+    console.log(humanScore + " your score");
+    console.log(computerScore + " computer's score");
+    if (humanScore > 4) {
+        humanScore = 0;
+        computerScore = 0;
+        console.log("You win the Game!!!")
+        console.log("starting new game now");
+        console.log("");
+    } else if (computerScore > 4) {
+        humanScore = 0;
+        computerScore = 0;
+        console.log("you lose")
+        console.log("starting new game now");
+        console.log("");
     }
-}
+});
+
+paper.addEventListener("click", () => {
+    humanChoice = "paper";
+    const computerChoice = getComputerChoice();
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`The computer chose: ${computerChoice}`);
+    console.log(playRound(humanChoice, computerChoice));
+    console.log(humanScore + " your score");
+    console.log(computerScore + " computer's score");
+    if (humanScore > 4) {
+        humanScore = 0;
+        computerScore = 0;
+        console.log("You win the Game!!!")
+        console.log("starting new game now");
+        console.log("");
+    } else if (computerScore > 4) {
+        humanScore = 0;
+        computerScore = 0;
+        console.log("you lose")
+        console.log("starting new game now");
+        console.log("");
+    }
+});
+
+scissors.addEventListener("click", () => {
+    humanChoice = "scissors";
+    const computerChoice = getComputerChoice();
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`The computer chose: ${computerChoice}`);
+    console.log(playRound(humanChoice, computerChoice));
+    console.log(humanScore + " your score");
+    console.log(computerScore + " computer's score");
+    if (humanScore > 4) {
+        humanScore = 0;
+        computerScore = 0;
+        console.log("You win the Game!!!")
+        console.log("starting new game now");
+        console.log("");
+    } else if (computerScore > 4) {
+        humanScore = 0;
+        computerScore = 0;
+        console.log("you lose")
+        console.log("starting new game now");
+        console.log("");
+    }
+});
 
 
-playGame();
-if (humanScore > computerScore) {
-    console.log("You win the Game!!!")
-} else if (computerScore > humanScore) {
-    console.log("you lose refresh the page to try again")
-} else console.log("it's a tie refresh the page to try again")
+
+
 
 
 
